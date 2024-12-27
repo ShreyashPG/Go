07f9636a53bd409e81e0db2e8882e4f2e5d0bc28
Shreyash Ghanekar <shreyasghanekar35@gmail.com>
2024-12-27 22:53:23 +0530

    learnt about functions in go and played around it

--- helloWorld.go
+++ helloWorld.go
@@ -4,4 +4,35 @@ import "fmt";
 
 func main(){
 	fmt.Println("Hello World from Go")
+
+	s:= fmt.Sprintln("Hello  World 2nd time from Go")
+
+	fmt.Println(s)
+
+	// foo();
+	foo()
+
+	// a,b:=bar("James Bond", 38)
+	a, b:=bar("James Bond", 38)
+	fmt.Println(a);
+	fmt.Println(b, "is the age of James bond after 10 years")
+
+	fmt.Println(`Hello This 
+	can 
+	be  
+	written 
+	this way 
+	also `)
+}
+
+func foo (){
+	// fmt.Print("The foo is here")
+	fmt.Println("The foo is here")
+}
+
+func bar(x string , y int) (string , int ) {
+	// return 	fmt.Sprintln(x,"is here ! ");
+	return 	fmt.Sprint(x," is here ! "), y+10;
+
+
 }
